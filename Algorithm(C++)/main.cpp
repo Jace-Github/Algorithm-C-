7ba22f03063182d72a5f27f4f13a2741adb21174
Jace <jace.hong.cs@gmail.com>
2024-03-04 19:43:26 -0500

    Initial Commit

---
+++ Algorithm(C++)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithm(C++)
+//
+//  Created by Jace Hong on 3/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
